67622752461bc3e64e144215eb766154b25eca63
pawurb <contact@pawelurbanek.com>
2024-03-03 22:17:11 +0100

    Add readme and fix parser

--- lib/devloop/diff_parser.rb
+++ lib/devloop/diff_parser.rb
@@ -7,13 +7,13 @@ module Devloop
     def call(diff)
       lines = diff.split("\n")
       results = []
+      file = ""
       lines.each_with_index do |line, index|
         if line.start_with?("+++ b/")
           file = line[6..-1]
-          if lines[index + 1] && lines[index + 1].start_with?("@@ -")
-            line_number = lines[index + 1].split(" ")[1].split(",")[0][1..-1]
-            results << "#{file}:#{line_number}"
-          end
+        elsif line.start_with?("@@ -")
+          line_number = line.match(/@@ -(\d+)/)[1]
+          results << "#{file}:#{line_number}"
         end
       end
       results
--- spec/diff_parser_spec.rb
+++ spec/diff_parser_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 require "devloop/diff_parser"
 
 describe Devloop::DiffParser do
-  let(:diff) do
+  let(:diff1) do
     <<~DIFF
       diff --git a/spec/models/team_spec.rb b/spec/models/team_spec.rb
       index 19772f2a..a32824f9 100644
@@ -25,7 +25,25 @@ describe Devloop::DiffParser do
     DIFF
   end
 
+  let(:diff2) do
+    <<~DIFF
+          diff --git a/spec/models/team_spec.rb b/spec/models/team_spec.rb
+      index 19772f2a..9f614e20 100644
+      --- a/spec/models/team_spec.rb
+      +++ b/spec/models/team_spec.rb
+      @@ -19 +19 @@ describe Team do
+      -    it "has_many emojis" do
+      +    it "has_many eojis" do
+      @@ -24,2 +24,2 @@ describe Team do
+      -  describe "normalize attributes" do
+      -    it "does not allow empty string values" do
+      +  describe "normalize attrbtes" do
+      +    it "does not allw empty sting values" do
+    DIFF
+  end
+
   it "parses the diff correctly" do
-    expect(Devloop::DiffParser.call(diff)).to eq(["spec/models/team_spec.rb:10", "spec/models/user_spec.rb:167"])
+    expect(Devloop::DiffParser.call(diff1)).to eq(["spec/models/team_spec.rb:10", "spec/models/user_spec.rb:167"])
+    expect(Devloop::DiffParser.call(diff2)).to eq(["spec/models/team_spec.rb:19", "spec/models/team_spec.rb:24"])
   end
 end
